6208378d06b3dc0b8d29367c4479a7dc35025590
NKID00 <nkid00@126.com>
2021-07-09 10:39:39 +0800

    ✨自动跳过已经收藏的动画

--- update.py
+++ update.py
@@ -9,7 +9,7 @@ from bilibili_api.user import User, BangumiType
 from bilibili_api.bangumi import get_meta
 from bilibili_api.exceptions import ResponseCodeException
 
-from config import READ_ONLY, OPEN_FAILED_BANGUMI_BILI_PAGE
+from config import SKIP_COLLECTED, READ_ONLY, OPEN_FAILED_BANGUMI_BILI_PAGE
 from utilities import (
     loop, client, print_debug, print_status,
     try_for_times_async_chain, try_for_times_async_json
@@ -63,39 +63,57 @@ async def update_one_bgm_data(
     bgm_id: int, status: str
 ):
     '''更新单个 Bangumi 动画数据'''
-    if status == 'collect':  # 看过 -> 更新分集进度
-        # 获取分集总数
-        subject_data = await try_for_times_async_json(  # 尝试三次
+    update_flag = True
+    if SKIP_COLLECTED:
+        # 获取当前收藏状态
+        response = await try_for_times_async_chain(  # 尝试三次
             3,
             lambda: client.get(
-                f'https://api.bgm.tv/subject/{bgm_id}',
+                f'https://api.bgm.tv/collection/{bgm_id}',
                 headers={'Authorization': data.bgm_auth_data}
             )
         )
-        eps_count = subject_data['eps_count']
-        print_debug(f'更新分集进度 @ {bgm_id} -> {eps_count} ...')
+        if response.status == 200:
+            collection_status = (await response.json())["status"]["type"]
+            if collection_status == status:
+                update_flag = False
+
+    if update_flag:
+        if status == 'collect':  # 看过 -> 更新分集进度
+            # 获取分集总数
+            subject_data = await try_for_times_async_json(  # 尝试三次
+                3,
+                lambda: client.get(
+                    f'https://api.bgm.tv/subject/{bgm_id}',
+                    headers={'Authorization': data.bgm_auth_data}
+                )
+            )
+            eps_count = subject_data['eps_count']
+            print_debug(f'更新分集进度 @ {bgm_id} -> {eps_count} ...')
+            if not READ_ONLY:
+                response = await try_for_times_async_chain(  # 尝试三次
+                    3,
+                    lambda: client.post(
+                        f'https://api.bgm.tv'
+                        f'/subject/{bgm_id}/update/watched_eps',
+                        data={'watched_eps': eps_count},
+                        headers={'Authorization': data.bgm_auth_data}
+                    )
+                )
+                response.raise_for_status()
+        print_debug(f'更新收藏 @ {bgm_id} -> {status} ...')
         if not READ_ONLY:
             response = await try_for_times_async_chain(  # 尝试三次
                 3,
                 lambda: client.post(
-                    f'https://api.bgm.tv'
-                    f'/subject/{bgm_id}/update/watched_eps',
-                    data={'watched_eps': eps_count},
+                    f'https://api.bgm.tv/collection/{bgm_id}/update',
+                    data={'status': status},
                     headers={'Authorization': data.bgm_auth_data}
                 )
             )
             response.raise_for_status()
-    print_debug(f'更新收藏 @ {bgm_id} -> {status} ...')
-    if not READ_ONLY:
-        response = await try_for_times_async_chain(  # 尝试三次
-            3,
-            lambda: client.post(
-                f'https://api.bgm.tv/collection/{bgm_id}/update',
-                data={'status': status},
-                headers={'Authorization': data.bgm_auth_data}
-            )
-        )
-        response.raise_for_status()
+    else:
+        print_debug(f'跳过 @ {bgm_id} -> {status} ...')
     data.bangumi_processed_count += 1
     print_debug(f'完成 @ {bgm_id}！')
 
